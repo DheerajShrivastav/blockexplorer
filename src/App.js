9085798eb18c6d94d449e7cb15a9dc8f42f04f2c
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2023-09-05 14:31:35 +0530

    added the functionality of transaction details

--- src/App.js
+++ src/App.js
@@ -1,36 +1,126 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from 'alchemy-sdk'
+import { useEffect, useState } from 'react'
 
-import './App.css';
+import './App.css'
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
-};
+}
 
+const alchemy = new Alchemy(settings)
 
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-const alchemy = new Alchemy(settings);
+function TransactionDetails({ txHash, onClose }) {
+  const [transactionInfo, setTransactionInfo] = useState(null)
+
+  useEffect(() => {
+    async function fetchTransactionInfo() {
+      const info = await alchemy.core.getTransactionReceipt(txHash)
+      setTransactionInfo(info)
+    }
+
+    fetchTransactionInfo()
+  }, [txHash])
+
+  return (
+    <div className="transaction-details">
+      <h2>Transaction Details</h2>
+      {transactionInfo ? (
+        <>
+          <div> Transaction Hash: {transactionInfo.transactionHash}</div>
+          <div> To: {transactionInfo.to}</div>
+          <div> From: {transactionInfo.from}</div>
+          <div> Block Number: {transactionInfo.blockNumber}</div>
+          <div> Contract Address: {transactionInfo.contractAddress ? transactionInfo.contractAddress  : "null"}</div>
+          <div> Gas Used: {transactionInfo.gasUsed._hex}</div>
+          <div> Effective Gas Price: {transactionInfo.effectiveGasPrice._hex}</div>
+          <div> Transaction Index: {transactionInfo.transactionIndex}</div>
+          <div> Type: {transactionInfo.type}</div>
+          {/* Add more transaction details as needed */}
+          {console.log(transactionInfo)}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={onClose}>Close</button>
+    </div>
+  )
+}
 
 function App() {
-  const [blockNumber, setBlockNumber] = useState();
+  const [blockNumber, setBlockNumber] = useState()
+  const [response, setResponse] = useState({})
+  const [transactions, setTransactions] = useState([])
+  const [selectedTxHash, setSelectedTxHash] = useState(null)
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      setBlockNumber(await alchemy.core.getBlockNumber())
     }
 
-    getBlockNumber();
-  });
+    getBlockNumber()
+  }, [])
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  useEffect(() => {
+    async function fetchData() {
+      const block = await alchemy.core.getBlock(blockNumber)
+      setResponse(block)
+    }
+
+    async function fetchTransactions() {
+      const tx = await alchemy.core.getBlockWithTransactions(blockNumber)
+      setTransactions(tx.transactions || [])
+    }
+
+    fetchData()
+    fetchTransactions()
+  }, [blockNumber])
+
+  const openTransactionInfo = (txHash) => {
+    setSelectedTxHash(txHash)
+  }
+
+  const closeTransactionInfo = () => {
+    setSelectedTxHash(null)
+  }
+
+  return (
+    <div>
+      <div className="App">Block Number: {blockNumber}</div>
+      <div>Hash: {response.hash}</div>
+      <div>ParentHash: {response.parentHash}</div>
+      <div>Number: {response.number}</div>
+      <div>TimeStamp: {response.timestamp}</div>
+      <div>Nonce: {response.nonce}</div>
+      <div>Difficulty: {response.difficulty}</div>
+      <div>Miner: {response.miner}</div>
+
+      <h2>Transactions:</h2>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <li key={index}>
+            <div>Transaction {index + 1}</div>
+            <div onClick={() => openTransactionInfo(transaction.hash)}>
+              Hash: {transaction.hash}
+            </div>
+            <div>Type: {transaction.type}</div>
+            <div>Block Hash: {transaction.blockHash}</div>
+            <div>From: {transaction.from}</div>
+            {/* Add more attributes as needed */}
+            <br />
+          </li>
+        ))}
+        <br />
+      </ul>
+
+      {selectedTxHash && (
+        <TransactionDetails
+          txHash={selectedTxHash}
+          onClose={closeTransactionInfo}
+        />
+      )}
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/GetBlockInfo.js
@@ -0,0 +1,14 @@
+// import { Alchemy, Network } from 'alchemy-sdk'
+// import React, { useEffect, useState } from 'react'
+// import reactRouterDom from 'react-router-dom'
+
+
+export default async function GetBlockInfo(props) {
+    // const [response, setResponse] = useState([])
+    //  useEffect(() => {
+    //     setResponse(prevResponse => prevResponse = props.alchemy.core.getBlock(props.blockNumber))
+    // }, [props.blockNumber])
+//     let response = await props.alchemy.core.getBlock(props.blockNumber)
+
+//     console.log(response)
+}
